094a12f46ff70e4d7c2dcdc7e4c54b06eb981fe8
José Manuel Luque <jluqgon214@g.educaand.es>
2025-02-12 17:37:39 +0100

    Creación Proyecto

---
+++ bin/uso_de_clases_dart.dart
@@ -0,0 +1,5 @@
+import 'package:uso_de_clases_dart/uso_de_clases_dart.dart' as uso_de_clases_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${uso_de_clases_dart.calculate()}!');
+}
---
+++ test/uso_de_clases_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:uso_de_clases_dart/uso_de_clases_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
